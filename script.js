39410c82f041bc5d52aa8146f5a59ce808b6b174
noahorror708 <noahorror708@gmail.com>
2025-08-05 12:49:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const mapDiv = document.getElementById("map");
-  mapDiv.innerHTML = "<p>這裡未來會放地圖 (Google Maps / Leaflet)</p>";
-});
+body {
+  font-family: "Helvetica", "Arial", sans-serif;
+  margin: 0;
+  padding: 0;
+  background: #111;
+  color: #eee;
+  text-align: center;
+}
+
+h1 {
+  background: #222;
+  padding: 1rem;
+  margin: 0;
+}
+
+#map {
+  width: 100%;
+  height: 500px;
+  background: #333;
+  margin-top: 1rem;
+}
